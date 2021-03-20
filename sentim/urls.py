e2a04b0cccf8c3bdbc076a376e8ca4df70e97a60
MaggoSahab <80995530+MaggoSahab@users.noreply.github.com>
2021-03-20 12:43:10 -0400

    Add files via upload

--- sentim/urls.py
+++ sentim/urls.py
@@ -4,6 +4,6 @@ from django.contrib import admin
 from .import views
 
 
-urlpatterns=[
-      url(r'^$',views.IndexView.as_view(),name='index'),
+urlpatterns = [
+      url(r'^$', views.IndexView.as_view(), name='index'),
       ]
